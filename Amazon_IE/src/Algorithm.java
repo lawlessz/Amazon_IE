4b528c044424406b1fafed5e432af67b374f09e6
Zach <lawlessz@uw.edu>
2017-04-02 16:50:24 -0700

    4/2

--- Amazon_IE/src/Algorithm.java
+++ Amazon_IE/src/Algorithm.java
@@ -2,11 +2,21 @@
 //
 public class Algorithm {
 
+	public int coordPass = 0;
 	
 	//find empty spots and enact procedure
 	public static void find_Empty(Grid g, Item rect) {
 		for (int i = 0; i<g.xdimen; i++) {
-			
+			for (int j = 0; j<g.ydimen; j++){
+				if(g.table[i][j] == 0){//if empty then we look for complete space
+					
+				}
+			}
 		}
+	}
+	
+	public static void space_Item(int coordX, int coordY, Grid g, Item rect){
+		
+		
 	}
 }
